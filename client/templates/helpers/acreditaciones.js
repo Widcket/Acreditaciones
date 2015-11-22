09673bf9329a5db5fd19dd10cabc8b604d4c0731
Rita Zerrizuela <zeta@widcket.com>
2015-11-22 00:49:40 -0300

    Update acreditaciones.js

--- client/templates/helpers/acreditaciones.js
+++ client/templates/helpers/acreditaciones.js
@@ -61,7 +61,7 @@ Template.inputAcreditar.onRendered(function () {
             for (var j = 0; j < keys.length; j++) {
               items[j] = unsorted[keys[j]];
             }
-            result = {};
+            var result = {};
             result["results"] = items;
             return result;
           }
